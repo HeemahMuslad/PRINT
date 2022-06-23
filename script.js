9fad6fd7a63d89ec1d794fc433acdefc3b055111
rohimat mustapha <rohimat.m1800276@st.futminna.edu.ng>
2022-06-23 13:20:41 +0100

    PRINT

---
+++ script.js
@@ -0,0 +1,7 @@
+alert( 'Hello World' );
+var n,h,c;
+n =prompt( "Enter Your Name" );
+h = prompt( "Enter Your Height" );
+c = prompt( " Enter Your Country" );
+document.write( n +"<br>" + h + "<br>" + c);
+
