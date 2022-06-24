2d185b9754786886989416efabf10ef3e5cf5ae1
rohimat mustapha <rohimat.m1800276@st.futminna.edu.ng>
2022-06-24 14:40:27 +0100

    PRINT

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ var n,h,c;
 n =prompt( "Enter Your Name" );
 h = prompt( "Enter Your Height" );
 c = prompt( " Enter Your Country" );
-document.write( n +"<br>" + h + "<br>" + c);
+console.log(n + "<br>" + h + "<br>" + c);
+document.write( n +"<br>" + h + "<br>" + c );
 
