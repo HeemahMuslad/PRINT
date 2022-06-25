da0fabe95a2e8c4d4c75c174790cb61590e48b60
rohimat mustapha <rohimat.m1800276@st.futminna.edu.ng>
2022-06-25 20:00:14 +0100

    CALC

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ var n,h,c;
 n =prompt( "Enter Your Name" );
 h = prompt( "Enter Your Height" );
 c = prompt( " Enter Your Country" );
-console.log(n + "<br>" + h + "<br>" + c);
-document.write( n +"<br>" + h + "<br>" + c );
+alert(n + "\n" + h + "\n" + c);
+
 
